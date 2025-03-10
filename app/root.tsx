f548d8b4a766ba549dc2f409dbc8a453c0dde55d
edpau <edpau@me.com>
2025-03-10 15:57:15 +0000

    feat: add nunito-sans font to root stylesheet

--- app/root.tsx
+++ app/root.tsx
@@ -1,28 +1,33 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Links, LiveReload, Scripts } from "@remix-run/react";
-import faviconAssetUrl from "./assets/favicon.svg";
+import type { LinksFunction } from '@remix-run/node'
+import { Links, LiveReload, Scripts } from '@remix-run/react'
+import faviconAssetUrl from './assets/favicon.svg'
+import fontStylesheetUrl from './styles/font.css'
 
 export const links: LinksFunction = () => {
-  return [
-    {
-      rel: "icon",
-      href: faviconAssetUrl,
-      type: "image/svg+xml",
-    },
-  ];
-};
+	return [
+		{
+			rel: 'icon',
+			href: faviconAssetUrl,
+			type: 'image/svg+xml',
+		},
+		{
+			rel: 'stylesheet',
+			href: fontStylesheetUrl,
+		},
+	]
+}
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Links />
-      </head>
-      <body>
-        <p>Hello World</p>
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Links />
+			</head>
+			<body>
+				<p>Hello World</p>
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
 }
