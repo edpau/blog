d95c4caec726598385f026da23ee9cdef6976266
edpau <edpau@me.com>
2025-03-12 15:04:07 +0000

    feat: enhance root layout with dynamic data, structured UI, and improved styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,16 @@
-import type { LinksFunction } from '@remix-run/node'
-import { Links, LiveReload, Scripts } from '@remix-run/react'
+import os from 'node:os'
 import { cssBundleHref } from '@remix-run/css-bundle'
+import { json, type LinksFunction } from '@remix-run/node'
+import {
+	Links,
+	LiveReload,
+	Outlet,
+	Scripts,
+	useLoaderData,
+} from '@remix-run/react'
 
 import faviconAssetUrl from './assets/favicon.svg'
 import fontStylesheetUrl from './styles/font.css'
-import './styles/global.css'
 import tailwindStyleSheetUrl from './styles/tailwind.css'
 
 export const links: LinksFunction = () => {
@@ -23,14 +29,40 @@ export const links: LinksFunction = () => {
 	].filter(Boolean)
 }
 
+export async function loader() {
+	return json({ username: os.userInfo().username })
+}
+
 export default function App() {
+	const data = useLoaderData<typeof loader>()
 	return (
-		<html lang="en">
+		<html lang="en" className="h-full overflow-hidden">
 			<head>
 				<Links />
 			</head>
-			<body>
-				<p className="p-8 text-xl">Hello World</p>
+			<body className="bg-background text-foreground flex h-full flex-col justify-between">
+				<header className="container mx-auto py-6">
+					<nav className="flex justify-between">
+						<div>
+							<div className="font-light">epic</div>
+							<div className="font-bold">notes</div>
+						</div>
+					</nav>
+				</header>
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container mx-auto flex justify-between">
+					<div>
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</div>
+					<p>Built with ♥️ by {data.username}</p>
+				</div>
+				<div className="h-5" />
+
 				<Scripts />
 				<LiveReload />
 			</body>
